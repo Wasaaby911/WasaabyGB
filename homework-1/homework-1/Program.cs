3f4339efb6cc4d4bcd42caf2c176c89b55c92d47
wasaaby911 <Wasaaby911@gmail.com>
2022-03-02 14:53:10 +0500

    1 задание

---
+++ homework-1/homework-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Now}");
+        }
+    }
+}
